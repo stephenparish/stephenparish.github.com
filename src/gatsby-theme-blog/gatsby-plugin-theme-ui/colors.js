3cc56833ee12819d9aaa62f086647a320f67332e
Stephen Parish <840677+stephenparish@users.noreply.github.com>
2019-11-29 08:10:45 -0800

    Update colors to sage theme

--- src/gatsby-theme-blog/gatsby-plugin-theme-ui/colors.js
+++ src/gatsby-theme-blog/gatsby-plugin-theme-ui/colors.js
@@ -1,26 +1,27 @@
 import merge from 'deepmerge'
 import defaultThemeColors from 'gatsby-theme-blog/src/gatsby-plugin-theme-ui/colors'
 
-/*
- * Want to change your theme colors?
- * Try uncommenting the color overrides below
- * to go from default purple to a blue theme
- */
-
-const darkBlue = `#007acc`
-const lightBlue = `#66E0FF`
-const blueGray = `#282c35`
-
 export default merge(defaultThemeColors, {
   useColorSchemeMediaQuery: true,
-  text: blueGray,
-  primary: darkBlue,
-  heading: blueGray,
+  text: '#1D202A',
+  primary: '#587768',
+  accent: '#397367',
   modes: {
     dark: {
-      background: blueGray,
-      primary: lightBlue,
-      highlight: lightBlue,
+      text: '#FFF',
+      background: '#1D202A',
+      primary: '#7EAA95',
+      accent: '#42858C',
     },
   },
 })
+
+// Other colors worth using in this palette:
+// #8abba3
+// #397367
+// #42858C
+// #35393c
+// #EDAE49 or #DCE2AA or #4B3B47 or #35524A or #A57548
+// error #E4572E or #DD6E42
+// light success #A0EEC0
+// For light #DCDCDD
